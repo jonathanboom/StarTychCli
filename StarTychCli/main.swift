978561f571894eecdadc6bb210ad4da6ac7dc084
Jonathan Lynch <303310+jonathanboom@users.noreply.github.com>
2020-01-25 18:15:17 -0500

    Added border color handling

--- StarTychCli/main.swift
+++ StarTychCli/main.swift
@@ -10,14 +10,32 @@ import Foundation
 import ImageFileManager
 import StarTych
 
+func hexCodeToColor(_ hexCode: String) -> CGColor? {
+    if hexCode.count != 6 {
+        return nil
+    }
+    
+    let scanner = Scanner(string: hexCode)
+    var hexNumber: UInt64 = 0
+    if scanner.scanHexInt64(&hexNumber) {
+        let components = [
+            CGFloat((hexNumber & 0xff0000) >> 16) / 255, // Red
+            CGFloat((hexNumber & 0x00ff00) >> 8) / 255, // Green
+            CGFloat(hexNumber & 0x0000ff) / 255, // Blue
+            1.0 // Alpha
+        ]
+        return CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: components)
+    }
+    
+    return nil
+}
+
 if CommandLine.arguments.count < 4 {
     print("\(CommandLine.arguments[0]) output color image-path [second-image-path ...]")
     exit(0)
 }
 
 let tych = StarTych(borderWeight: 0.04)
-
-// TODO: Ignoring border color arg for now
 
 for i in 3 ..< CommandLine.arguments.count {
     let inputUrl = URL(fileURLWithPath: CommandLine.arguments[i])
@@ -28,7 +46,18 @@ for i in 3 ..< CommandLine.arguments.count {
     }
 }
 
-// TODO: Make full tych
+let colorArg = CommandLine.arguments[2]
+if let color = hexCodeToColor(colorArg) {
+    tych.borderColor = color
+} else {
+    print("Invalid hex code for border color, trying average color")
+    if let averageColor = tych.averageColor {
+        tych.borderColor = averageColor
+    } else {
+        print("Couldn't compute average color, just using whatever")
+    }
+}
+
 guard let finalImage = tych.makeImage() else {
     print("Could not make *tych")
     exit(1)
