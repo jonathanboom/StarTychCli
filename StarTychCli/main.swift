ccba8dd1aa5b9ddbadfbf6d9cdc43d7121d8f015
Jonathan Lynch <303310+jonathanboom@users.noreply.github.com>
2020-01-25 14:30:31 -0500

    Basic command line interface to StarTych

--- StarTychCli/main.swift
+++ StarTychCli/main.swift
@@ -7,6 +7,32 @@
 //
 
 import Foundation
+import ImageFileManager
+import StarTych
 
-print("Hello, World!")
+if CommandLine.arguments.count < 4 {
+    print("\(CommandLine.arguments[0]) output color image-path [second-image-path ...]")
+    exit(0)
+}
 
+let tych = StarTych(borderWeight: 0.04)
+
+// TODO: Ignoring border color arg for now
+
+for i in 3 ..< CommandLine.arguments.count {
+    let inputUrl = URL(fileURLWithPath: CommandLine.arguments[i])
+    if let image = ImageFileManager.createCGImage(from: inputUrl) {
+        tych.addImage(image)
+    } else {
+        print("Could not load image at \(inputUrl)")
+    }
+}
+
+// TODO: Make full tych
+guard let finalImage = tych.makeStarTych() else {
+    print("Could make *tych")
+    exit(1)
+}
+
+let outputUrl = URL(fileURLWithPath: CommandLine.arguments[1])
+ImageFileManager.write(image: finalImage, to: outputUrl)
