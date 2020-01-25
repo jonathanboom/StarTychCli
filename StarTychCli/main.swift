c109b31fbb14488c76ebae48b120a619da8bdeef
Jonathan Lynch <303310+jonathanboom@users.noreply.github.com>
2020-01-25 13:26:12 -0500

    Initial Commit

---
+++ StarTychCli/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StarTychCli
+//
+//  Created by Jonathan Lynch on 1/25/20.
+//  Copyright © 2020 Intuitive Soup. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
